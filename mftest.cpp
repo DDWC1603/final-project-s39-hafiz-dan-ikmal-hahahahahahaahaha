249f771bc14a41150b9d87fa51e229a9b49ad77b
muhdhafiz2169 <33885159+muhdhafiz2169@users.noreply.github.com>
2018-01-29 09:30:35 +0800

    fixed version (laki pompuan) heheheheee

---
+++ mftest.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+struct profile
+{
+	int x=10;
+	string name1="Maznah";
+	bool gender;
+	void apaapanama() {
+		if(gender==false)
+		{
+			cout<<endl<<"Male";
+		}
+		else if(gender==true)
+		{
+			cout<<endl<<"Female";
+		}
+	}
+};
+		
+int main() {	
+	char input;
+	profile user[10];
+	bool holder;
+	int cm=0, cf=0;
+		
+	for(int x=0; x<10; x++){
+			
+		cout<<"Enter Your Gender : "<<endl;
+		cin>>input;
+			
+		if(input=='m') {
+			user[x].gender=false;			
+			cm++;
+		}
+			
+		else if(input=='f') {
+			user[x].gender=true;
+			cf++;
+		}
+			
+		else {					// repeatable
+			cout<<"BODOH";	
+		}
+	}
+		
+	for(int y=0; y<cm; y++) {
+		user[y].gender = false;
+		user[y].apaapanama();
+	}
+		
+	for(cm; cm<10; cm++) {
+		user[cm].gender = true;
+		user[cm].apaapanama();
+	}
+}
